725395b70c9e35ed802555646ec8fd76e15f5a76
alexey.lazarenko <dzotic9@gmail.com>
2019-02-14 10:52:58 +0200

    JE-45145

--- scripts/phpvertest.sh
+++ scripts/phpvertest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 a=`php -v |head -n1 |awk '{print $2}'|cut -f 1,2 -d .`;
 
-sed -i '$ a\[ioncube\]\n;zend_extension=/usr/lib64/php/modules/ioncube/ioncube_loader_lin_5.3.so\n\;zend_extension=/usr/lib64/php/modules/ioncube/ioncube_loader_lin_5.4.so\n\;zend_extension=/usr/lib64/php/modules/ioncube/ioncube_loader_lin_5.5.so\n\;zend_extension=/usr/lib64/php/modules/ioncube/ioncube_loader_lin_5.6.so\n\;zend_extension=/usr/lib64/php/modules/ioncube/ioncube_loader_lin_7.0.so\n\;zend_extension=/usr/lib64/php/modules/ioncube/ioncube_loader_lin_7.1.so\n\;zend_extension=/usr/lib64/php/modules/ioncube/ioncube_loader_lin_7.2.so' /etc/php.ini ;
+sed -i '$ a\[ioncube\]\n;zend_extension=/usr/lib64/php/modules/ioncube/ioncube_loader_lin_5.3.so\n\;zend_extension=/usr/lib64/php/modules/ioncube/ioncube_loader_lin_5.4.so\n\;zend_extension=/usr/lib64/php/modules/ioncube/ioncube_loader_lin_5.5.so\n\;zend_extension=/usr/lib64/php/modules/ioncube/ioncube_loader_lin_5.6.so\n\;zend_extension=/usr/lib64/php/modules/ioncube/ioncube_loader_lin_7.0.so\n\;zend_extension=/usr/lib64/php/modules/ioncube/ioncube_loader_lin_7.1.so\n\;zend_extension=/usr/lib64/php/modules/ioncube/ioncube_loader_lin_7.2.so;zend_extension=/usr/lib64/php/modules/ioncube/ioncube_loader_lin_7.3.so' /etc/php.ini ;
 
 ioncube_loader_line=$(echo $(grep $a'.so' /etc/php.ini) | sed 's/^.//');
 sed -i "s|;$ioncube_loader_line|$ioncube_loader_line|g" /etc/php.ini
